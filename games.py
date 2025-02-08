4b14d56edb4553a973d79ed4d56feda9d20cfc77
Fireweld Tesfaye <freyesus12@gmail.com>
2025-02-08 18:07:29 +0300

    solve games problem in codeforce

---
+++ games.py
@@ -0,0 +1,17 @@
+game = int(input())
+
+home = []
+away = []
+
+for i in range(game):
+    f,m = map(int,input().split())
+    home.append(f)
+    away.append(m)
+
+counter = 0
+for i in range(game)unter += 1
+print(counter):
+    for j in range(game):
+        if i!=j:
+            if home[i] == away[j]:
+                co
